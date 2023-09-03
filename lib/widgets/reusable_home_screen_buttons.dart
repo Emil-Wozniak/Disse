3f45f1a7eec93fa57493a02e201cd6e8c7e6be37
ShiroganeS <104523638+ShiroganeS@users.noreply.github.com>
2023-09-03 17:18:36 +0200

    DIS-5-improve reusable & editable screen-button

--- lib/widgets/reusable_home_screen_buttons.dart
+++ lib/widgets/reusable_home_screen_buttons.dart
@@ -45,34 +45,3 @@ class ReusableHomeButton extends StatelessWidget {
 
 
 
-// (
-//     BuildContext context, String text, Function onTap) {
-//   return Container(
-//     width: MediaQuery.of(context).size.width,
-//     height: 50,
-//     margin: const EdgeInsets.fromLTRB(0, 10, 0, 20),
-//     decoration: BoxDecoration(borderRadius: BorderRadius.circular(90)),
-//     child: ElevatedButton(
-//       onPressed: () {
-//         onTap();
-//       },
-//       style: ElevatedButton.styleFrom(
-//           backgroundColor: Colors.transparent,
-//           shadowColor: Colors.transparent,
-          
-        
-//         shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-//           RoundedRectangleBorder(
-//             borderRadius: BorderRadius.circular(15),
-//           ),
-//         ),
-//       ),
-      
-//       child: Text(
-//         text,
-//         style: const TextStyle(
-//             color: Colors.black87, fontWeight: FontWeight.bold, fontSize: 16),
-//       ),
-//     ),
-//   );
-// }
