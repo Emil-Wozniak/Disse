1cb11f1935f180e2d7f3ca8f6d7a33c2a61c8b34
EwaBaczynska <baczynska.ewa@gmail.com>
2023-09-17 19:39:47 +0200

    DIS-11-user_colors

---
+++ lib/utils/user_colors.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+const green = Colors.green;
+const lightGreen = Colors.lightGreen;
+const lime = Colors.lime;
+const yellow = Colors.yellow;
+const amber = Colors.amber;
+const orange = Colors.orange;
+const deepOrange = Colors.deepOrange;
+const red = Colors.red;
+const pink = Colors.pink;
+const purple = Colors.purple;
+const deepPurple = Colors.deepPurple;
+const cyan = Colors.cyan;
+const blue = Colors.blue;
+const lightBlue = Colors.lightBlue;
+const grey = Colors.grey;
+const brown = Colors.brown;
+const white = Colors.white;
