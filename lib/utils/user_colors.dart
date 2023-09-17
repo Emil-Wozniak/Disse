165a7c3b9e9819ba366dd18988edbf8b49c22bfb
EwaBaczynska <baczynska.ewa@gmail.com>
2023-09-17 21:19:18 +0200

    DIS-11-List_user_colors

--- lib/utils/user_colors.dart
+++ lib/utils/user_colors.dart
@@ -1,19 +1,21 @@
 import 'package:flutter/material.dart';
 
-const green = Colors.green;
-const lightGreen = Colors.lightGreen;
-const lime = Colors.lime;
-const yellow = Colors.yellow;
-const amber = Colors.amber;
-const orange = Colors.orange;
-const deepOrange = Colors.deepOrange;
-const red = Colors.red;
-const pink = Colors.pink;
-const purple = Colors.purple;
-const deepPurple = Colors.deepPurple;
-const cyan = Colors.cyan;
-const blue = Colors.blue;
-const lightBlue = Colors.lightBlue;
-const grey = Colors.grey;
-const brown = Colors.brown;
-const white = Colors.white;
+List userColors = const [
+  Colors.green,
+  Colors.lightGreen,
+  Colors.lime,
+  Colors.yellow,
+  Colors.amber,
+  Colors.orange,
+  Colors.deepOrange,
+  Colors.red,
+  Colors.pink,
+  Colors.purple,
+  Colors.deepPurple,
+  Colors.cyan,
+  Colors.blue,
+  Colors.lightBlue,
+  Colors.grey,
+  Colors.brown,
+  Colors.white,
+];
